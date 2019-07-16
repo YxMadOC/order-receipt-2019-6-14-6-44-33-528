3047c5d875036061baab2a5b080f5bad828b4a37
Cayde Xu <cayde.xu@oocl.com>
2019-07-16 09:24:49 +0800

    Encapsulated getTax()

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -24,17 +24,17 @@ public class OrderReceipt {
         double totalSalesTax = 0d;
         double totalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
-            output.append(String.format("%s\t%s\t%s\t%s\n",lineItem.getDescription(),lineItem.getPrice(),lineItem.getQuantity(),lineItem.totalAmount()));
+            output.append(String.format("%s\t%s\t%s\t%s\n", lineItem.getDescription(), lineItem.getPrice(), lineItem.getQuantity(), lineItem.totalAmount()));
 
-            double salesTax = lineItem.totalAmount() * .10;
+            double salesTax = lineItem.getTax();
             totalSalesTax += salesTax;
 
             totalAmount += lineItem.totalAmount() + salesTax;
         }
 
         output.append("Sales Tax").append('\t').append(totalSalesTax);
-
         output.append("Total Amount").append('\t').append(totalAmount);
+
         return output.toString();
     }
 }
