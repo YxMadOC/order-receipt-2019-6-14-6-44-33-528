1a2cfc935eae31664927e59b9152727d6beee464
Cayde Xu <cayde.xu@oocl.com>
2019-07-16 09:23:29 +0800

    Refactored duplicated code.

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -22,26 +22,19 @@ public class OrderReceipt {
         output.append(order.getCustomerAddress());
 
         double totalSalesTax = 0d;
-        double total = 0d;
+        double totalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.totalAmount());
-            output.append('\n');
+            output.append(String.format("%s\t%s\t%s\t%s\n",lineItem.getDescription(),lineItem.getPrice(),lineItem.getQuantity(),lineItem.totalAmount()));
 
             double salesTax = lineItem.totalAmount() * .10;
             totalSalesTax += salesTax;
 
-            total += lineItem.totalAmount() + salesTax;
+            totalAmount += lineItem.totalAmount() + salesTax;
         }
 
         output.append("Sales Tax").append('\t').append(totalSalesTax);
 
-        output.append("Total Amount").append('\t').append(total);
+        output.append("Total Amount").append('\t').append(totalAmount);
         return output.toString();
     }
 }
