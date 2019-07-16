4996543702a43cc4711f48c2dbf6d42a98ccbe43
Cayde Xu <cayde.xu@oocl.com>
2019-07-16 09:29:29 +0800

    Simplified output content building.

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -15,26 +15,18 @@ public class OrderReceipt {
 
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
-
-        output.append("======Printing Orders======\n");
-
-        output.append(order.getCustomerName());
-        output.append(order.getCustomerAddress());
-
+        output.append(String.format("======Printing Orders======\n%s%s", order.getCustomerName(), order.getCustomerAddress()));
         double totalSalesTax = 0d;
         double totalAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             output.append(String.format("%s\t%s\t%s\t%s\n", lineItem.getDescription(), lineItem.getPrice(), lineItem.getQuantity(), lineItem.totalAmount()));
-
             double salesTax = lineItem.getTax();
             totalSalesTax += salesTax;
-
             totalAmount += lineItem.totalAmount() + salesTax;
         }
 
         output.append("Sales Tax").append('\t').append(totalSalesTax);
         output.append("Total Amount").append('\t').append(totalAmount);
-
         return output.toString();
     }
 }
